d2a96c7f7d4ad4f2ef4fba55ad4b84e5925c9f33
saritadc <dsarritta@gmail.com>
2020-07-09 20:27:48 +0545

    question 19 solved

---
+++ question19.py
@@ -0,0 +1,9 @@
+# 19. Write a Python program to get the smallest number from a list.
+
+def small_list_item(lists):
+    lists.sort()
+
+    return print(lists[0])
+
+small_list_item([9,3,4,1])
+    
