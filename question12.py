8a627e8a0ad067ca5a943999b43bf52676be388a
saritadc <dsarritta@gmail.com>
2020-07-09 15:24:03 +0545

    question 12 solved

---
+++ question12.py
@@ -0,0 +1,9 @@
+# 2. Write a Python script that takes input from the user and displays that input
+# back in upper and lower cases.
+
+sample = input("Enter s string: ")
+sample_upper_case = sample.upper()
+sample_lower_case = sample.lower()
+
+print("Upper case is: ", sample_upper_case)
+print("Lower case is: ", sample_lower_case)
