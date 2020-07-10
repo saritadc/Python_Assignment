2c2282f5aa64dabb6e684fdfd72511920535c674
saritadc <dsarritta@gmail.com>
2020-07-10 17:14:38 +0545

    question 31 is solved

---
+++ question31.py
@@ -0,0 +1,6 @@
+# 31. Write a Python program to iterate over dictionaries using for loops.
+
+dictionary = {1: 10, 2: 20, 3: 30, 4: 40, 5: 50, 6: 60}
+for key, value in dictionary.items():
+    print(key, value)
+
